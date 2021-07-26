51912f2c8b28b36905f19bb9997bda086928847a
MuresanCoralia <83501334+MuresanCoralia@users.noreply.github.com>
2021-07-26 17:37:30 +0300

    Add files via upload

---
+++ MiddleViewController.swift
@@ -0,0 +1,67 @@
+//
+//  MiddleViewController.swift
+//  MiddleViewController
+//
+//  Created by Wolfpack Digital on 26.07.2021.
+//
+
+import UIKit
+
+class MiddleViewController: UIViewController
+{
+
+    @IBOutlet weak var nothingHappened: UILabel!
+    
+    var eventNumber = 1;
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        addEvent(from: "viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+    
+    func addEvent(from: String)
+    {
+        if let existingText = nothingHappened.text
+        {
+            nothingHappened.text = "\(existingText)\nEvent number \(eventNumber) was \(from)"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        addEvent(from: "viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        addEvent(from: "viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool)
+    {
+        super.viewWillDisappear(animated)
+        addEvent(from: "viewWillDisapear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool)
+    {
+        super.viewDidDisappear(animated)
+        addEvent(from: "viewDidDisapear")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
